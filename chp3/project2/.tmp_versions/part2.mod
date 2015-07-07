c325fc6600cb72d4048d35219c45b914764c3031
kwagjj <kwagjj@kwagjj-Inspiron-3420>
2015-07-08 03:26:27 +0900

    part2-revised

--- chp3/project2/.tmp_versions/part2.mod
+++
@@ -1,2 +0,0 @@
-/home/kwagjj/projects/kernelprog/chp3/project2/part2.ko
-/home/kwagjj/projects/kernelprog/chp3/project2/part2.o
--- chp3/project2/part2-2.c
+++ chp3/project2/part2-2.c
@@ -1,42 +1,16 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/module.h>
-#include <string.h>
-
-stataic int depth=0;
-
-void shiftstr(char namebuf[30]){
-	int j=0;
-	for(j=0;j<29;j++){
-		namebuf[29-j]=namebuf[29-j-1];
-	}
-	
-}
 
 int print_children(struct task_struct *parent){
 
-	//printk("%10d%20s%10li\n",parent->pid,parent->comm,parent->state);	// first print out info of parent
-	printk("%10d",parent->pid);
-	char namebuf[30];
-	strcpy(namebuf,parent->comm);
-	int i=0;
-	for(i=0;i<depth;i++){
-		shiftstr(namebuf);
-		namebuf[0]='-';
-	}
-	pritnk("%20s%10li\n",namebuf,parent->state);
+	printk("%10d%20s%10li\n",parent->pid,parent->comm,parent->state);	// first print out info of parent
 
-	// parent info print finished.
-	//
-	//
-	//
 	struct task_struct *task;
 	struct list_head *list;
 	list_for_each(list,&(parent->children)){	// if there is no children, then it won't go any further
-		depth++;
 		task=list_entry(list,struct task_struct,sibling);	// returns pointer of struct_task
 		print_children(task);
-		depth--;
 
 	}
 
@@ -45,8 +19,9 @@ int print_children(struct task_struct *parent){
 
 int start(void){
 
+
 	printk("starting module\n");
-	printk("%10s%20s%10s\n","pid","task_name","state");
+	printk("pid\ttask_name\tstate\n");
 
 
 
